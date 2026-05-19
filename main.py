2f5d3dbcfc4fec2931e24b6ff229e075334e457c
Hiram5j8 <hiram5j8@gmail.com>
2026-05-19 22:02:25 +0800

    2605192200

--- main.py
+++ main.py
@@ -34,27 +34,22 @@ def home():
 
 @app.post("/process")
 async def process(base_xls: UploadFile = File(...), data_zip: UploadFile = File(...)):
-    import logging
-
     work = "/tmp/work"
-    work2 = "/tmp/work2"
     os.makedirs(work, exist_ok=True)
 
-    base_path = os.path.join(work, base_xls.filename)
-    zip_path = os.path.join(work2, data_zip.filename)
-    
-    #shutil.copy(master_file, out_file)
+    # 上傳檔案存起來
+    upload_path = os.path.join(work, base_xls.filename)
 
-    with open(base_path, "wb") as f:
+    with open(upload_path, "wb") as f:
         shutil.copyfileobj(base_xls.file, f)
-         
-    #base_df = pd.read_excel(base_path)
-    
-    base_path = os.path.join(work, "result.xlsx")
-    result_wb.save(base_path)
 
+    # 直接複製成 result.xlsx
+    result_path = os.path.join(work, "result.xlsx")
+    shutil.copy(upload_path, result_path)
+
+    # 回傳檔案
     return FileResponse(
-        base_path,
+        result_path,
         media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         filename="result.xlsx"
     )
